26a575ea183c5dbee7b88cdb5216f24edff5c717
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2022-12-11 18:23:43 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import './App.css';
+
+
+const API_URL = "https://raw.githubusercontent.com/ninjas-at-work/Sample-JSON/main/projects.json"
+const App = ()=>{
+    const searchProjects = async () =>{
+        const response = await fetch(`${API_URL}`)
+        const data = await response.json()
+        console.log(data.projects);
+    }
+    useEffect(()=>{
+    searchProjects();
+    },[])
+    return (
+        <div className='app'>
+        <h1>Portfolio Peek</h1>
+        <div className='search'>
+        <input placeholder='Search Projects' 
+        value = 'Project-A' onChange={()=>{
+
+        }}/>
+        <button id="searchbtn" onClick={()=>{}}>Search</button>
+       
+       <div class="main-container">
+       
+       </div>
+        </div>
+        </div>
+    );
+
+}
+
+export default App;
