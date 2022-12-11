78ede3abdc45bbe7b5d349a79e5f1a079cb0e3d7
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2022-12-11 18:26:42 -0500

    Basic template - with styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, {useEffect} from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-
-const API_URL = "https://raw.githubusercontent.com/ninjas-at-work/Sample-JSON/main/projects.json"
-const App = ()=>{
-    const searchProjects = async () =>{
-        const response = await fetch(`${API_URL}`)
-        const data = await response.json()
-        console.log(data.projects);
-    }
-    useEffect(()=>{
+const API_URL =
+  "https://raw.githubusercontent.com/ninjas-at-work/Sample-JSON/main/projects.json";
+const App = () => {
+  const searchProjects = async () => {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+    console.log(data.projects);
+  };
+  useEffect(() => {
     searchProjects();
-    },[])
-    return (
-        <div className='app'>
-        <h1>Portfolio Peek</h1>
-        <div className='search'>
-        <input placeholder='Search Projects' 
-        value = 'Project-A' onChange={()=>{
+  }, []);
+  return (
+    <div className="app">
+      <h1>Portfolio Peek</h1>
+      <div className="search">
+        <input
+          placeholder="Search Projects"
+          value="Project-A"
+          onChange={() => {}}
+        />
+        <button id="searchbtn" onClick={() => {}}>
+          Search
+        </button>
 
-        }}/>
-        <button id="searchbtn" onClick={()=>{}}>Search</button>
-       
-       <div class="main-container">
-       
-       </div>
-        </div>
-        </div>
-    );
-
-}
+      </div>
+      
+      <div class="main-container">
+        
+      <p> Main cards</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
